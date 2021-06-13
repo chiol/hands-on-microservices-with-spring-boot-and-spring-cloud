ac2cd0e5cf9b45e36d2d2be5585874ea6f46674c
chiol <jcy6677@gmail.com>
2021-06-13 15:51:21 +0900

    1. 골격 코드 생성

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd microservices/product-service;           ./gradlew build; cd -; \
+cd microservices/review-service;            ./gradlew build; cd -; \
+cd microservices/recommendation-service;    ./gradlew build; cd -; \
+cd microservices/product-composite-service; ./gradlew build; cd -;
---
+++ create-project.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+mkdir microservices
+cd microservices
+
+spring init \
+--boot-version=2.5.1.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=product-service \
+--package-name=io.github.chiol.microservices.core.product \
+--groupId=io.github.chiol.microservices.core.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-service
+
+spring init \
+--boot-version=2.5.1.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=review-service \
+--package-name=io.github.chiol.microservices.core.review \
+--groupId=io.github.chiol.microservices.core.review \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+review-service
+
+spring init \
+--boot-version=2.5.1.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=recommendation-service \
+--package-name=io.github.chiol.microservices.core.recommendation \
+--groupId=io.github.chiol.microservices.core.recommendation \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+recommendation-service
+
+spring init \
+--boot-version=2.5.1.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=product-composite-service \
+--package-name=io.github.chiol.microservices.composite.product \
+--groupId=io.github.chiol.microservices.composite.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-composite-service
+
+cd ..
