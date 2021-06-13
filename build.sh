69b1568e702937594601e498914b5a66827b0029
chiol <jcy6677@gmail.com>
2021-06-13 16:01:07 +0900

    2. 그래들에 멀티 프로젝트 빌드 설정

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
-cd microservices/product-service;           ./gradlew build; cd -; \
-cd microservices/review-service;            ./gradlew build; cd -; \
-cd microservices/recommendation-service;    ./gradlew build; cd -; \
-cd microservices/product-composite-service; ./gradlew build; cd -;
+cat <<EOF > settings.gradle
+include ':microservices:product-service'
+include ':microservices:review-service'
+include ':microservices:recommendation-service'
+include ':microservices:product-composite-service'
+EOF
+
+
+cp -r microservices/product-service/gradle .
+cp microservices/product-service/gradlew .
+cp microservices/product-service/gradlew.bat .
+cp microservices/product-service/.gitignore .
+
+find microservices -depth -name "gradle" -exec rm -rfv "{}" \;
+find microservices -depth -name "gradlew" -exec rm -rfv "{}" \;
+
+./gradlew build
