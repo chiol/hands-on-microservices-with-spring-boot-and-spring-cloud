c0ab7e558e21c02678fe136e48a24258ef873305
chiol <jcy6677@gmail.com>
2021-06-13 20:00:19 +0900

    4-1. api 구현 (product)

--- microservices/product-service/src/main/java/io/github/chiol/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/io/github/chiol/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.chiol.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("io.github.chiol")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
